1b6c2e9162d90702df9a5ecfa586776c75d3a424
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2025-03-13 12:38:23 +0530

    login with google and email varification completed ✨💥

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { AddUser, getAllUsers, getUserProfile, handleBlockUser, userLogin } from "../controllers/userController.js";
+import { AddUser, getAllUsers, getUserProfile, handleBlockUser, loginWithGoogle, sendOTP, userLogin, verifyOTP } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -8,7 +8,10 @@ userRouter.post("/login",userLogin);
 userRouter.get("/all",getAllUsers);
 userRouter.get("/block/:email",handleBlockUser);
 userRouter.get("/",getUserProfile);
+userRouter.post("/googlelogin",loginWithGoogle);
 
+userRouter.get("/sendOTP",sendOTP);
+userRouter.post("/verifyEmail",verifyOTP);
 
 
 export default userRouter;
